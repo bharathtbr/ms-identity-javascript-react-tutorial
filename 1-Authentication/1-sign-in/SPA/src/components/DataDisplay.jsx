47325f7fc30a7b9960062f46fd3a4a53d54792c9
Bharath Reddy T <48080915+bharathtbr@users.noreply.github.com>
2024-02-21 21:23:05 -0500

    Update DataDisplay.jsx

--- 1-Authentication/1-sign-in/SPA/src/components/DataDisplay.jsx
+++ 1-Authentication/1-sign-in/SPA/src/components/DataDisplay.jsx
@@ -1,16 +1,21 @@
+import React from 'react';
 import { Table } from 'react-bootstrap';
-import { createClaimsTable } from '../utils/claimUtils';
+import { createClaimsTable, ClaimTable } from '../utils/claimUtils'; // Assuming you have a type for the claims table
 
 import '../styles/App.css';
 
-export const IdTokenData = (props) => {
-    const tokenClaims = createClaimsTable(props.idTokenClaims);
+interface IdTokenDataProps {
+    idTokenClaims: any; // Adjust the type according to your actual data structure
+}
+
+const IdTokenData: React.FC<IdTokenDataProps> = (props) => {
+    const tokenClaims: ClaimTable = createClaimsTable(props.idTokenClaims); // Assuming ClaimTable is the correct type
 
     const tableRow = Object.keys(tokenClaims).map((key, index) => {
         return (
             <tr key={key}>
-                {tokenClaims[key].map((claimItem) => (
-                    <td key={claimItem}>{claimItem}</td>
+                {tokenClaims[key].map((claimItem, index) => (
+                    <td key={index}>{claimItem}</td>
                 ))}
             </tr>
         );
@@ -43,3 +48,5 @@ export const IdTokenData = (props) => {
         </>
     );
 };
+
+export default IdTokenData;
