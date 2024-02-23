0c44d1bcc9b3a25481100808a18e0299f7aa2ae7
Bharath Reddy T <48080915+bharathtbr@users.noreply.github.com>
2024-02-22 21:27:18 -0500

    Update NavigationBar.jsx

--- 1-Authentication/1-sign-in/SPA/src/components/NavigationBar.jsx
+++ 1-Authentication/1-sign-in/SPA/src/components/NavigationBar.jsx
@@ -1,52 +1,43 @@
+import React from 'react';
 import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
 import { Navbar, Dropdown, DropdownButton } from 'react-bootstrap';
 import { loginRequest } from '../authConfig';
+import { PublicClientApplication } from '@azure/msal-browser';
 
-export const NavigationBar = () => {
+export const NavigationBar: React.FC = () => {
     const { instance } = useMsal();
-
-    let activeAccount;
-
-    if (instance) {
-        activeAccount = instance.getActiveAccount();
-    }
+    const activeAccount = instance?.getActiveAccount();
 
     const handleLoginPopup = () => {
-        /**
-         * When using popup and silent APIs, we recommend setting the redirectUri to a blank page or a page
-         * that does not implement MSAL. Keep in mind that all redirect routes must be registered with the application
-         * For more information, please follow this link: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/login-user.md#redirecturi-considerations
-         */
         instance
-            .loginPopup({
+            ?.loginPopup({
                 ...loginRequest,
                 redirectUri: '/redirect',
             })
-            .catch((error) => console.log(error));
+            .catch((error: Error) => console.log(error));
     };
 
     const handleLoginRedirect = () => {
-        instance.loginRedirect(loginRequest).catch((error) => console.log(error));
+        instance
+            ?.loginRedirect(loginRequest)
+            .catch((error: Error) => console.log(error));
     };
 
     const handleLogoutPopup = () => {
         instance
-            .logoutPopup({
+            ?.logoutPopup({
                 mainWindowRedirectUri: '/', // redirects the top level app after logout
                 account: instance.getActiveAccount(),
             })
-            .catch((error) => console.log(error));
+            .catch((error: Error) => console.log(error));
     };
 
     const handleLogoutRedirect = () => {
-        instance.logoutRedirect().catch((error) => console.log(error));
+        instance
+            ?.logoutRedirect()
+            .catch((error: Error) => console.log(error));
     };
 
-    /**
-     * Most applications will need to conditionally render certain components based on whether a user is signed in or not.
-     * msal-react provides 2 easy ways to do this. AuthenticatedTemplate and UnauthenticatedTemplate components will
-     * only render their children if a user is authenticated or unauthenticated, respectively.
-     */
     return (
         <>
             <Navbar bg="primary" variant="dark" className="navbarStyle">
